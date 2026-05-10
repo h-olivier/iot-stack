ea3e0dd06d9bd47a150f2dbc7cb5bc64f9c438a9
h-olivier <randriamahatsarahenintsoaolivi@gmail.com>
2026-05-10 22:02:55 +0300

    add nodered/settings.js

---
+++ nodered/settigns.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2b$08$M9l9Q1Qe6jN6fXh5kK6Vnuh0eQ5X2G6r9e3mFf4wI2XjYQ7fN8vP2",
+            permissions: "*"
+        }]
+    },
+
+    uiPort: process.env.PORT || 1880,
+
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000,
+
+    debugMaxLength: 1000,
+
+    flowFile: 'flows.json',
+
+    credentialSecret: "iotstacksecret",
+
+    functionGlobalContext: {}
+}
